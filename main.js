adfb092514d6b0c750e7eae384f73e07b4a5eeb0
shishirasonice <82163635+shishirasonice@users.noreply.github.com>
2022-05-11 14:08:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;    
 
 function preload(){
     song1 = loadSound("hedwigs-theme-song.mp3");
@@ -11,9 +15,31 @@ function setup(){
     canvas.center;
 
     video = createCapture(VIDEO);
-     video.hide();
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialized');
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWristX.x;
+        leftWristY = results[0].pose.leftWristY.y;
+
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+        rightWristX = results[0].pose.rightWristX.x;
+        rightWristY = results[0].pose.rightWristY.y;
+         
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
